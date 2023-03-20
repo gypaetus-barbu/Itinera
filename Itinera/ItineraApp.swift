5c01845f33d91708287d9c004848bd4ad842ed6e
MonLuc2a <lucas.monribot@ynov.com>
2023-03-20 14:31:49 +0100

    feat(#1) init project

---
+++ Itinera/ItineraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItineraApp.swift
+//  Itinera
+//
+//  Created by Lucas on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ItineraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
